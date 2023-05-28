6457fbe81a0b10aa96185ef07ff6a25b65311f14
Mateo <mateo2823@hotmail.com>
2023-05-28 03:18:39 -0500

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { fetchDataFromApi } from './util/tmdb_api'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice'
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/SearchResults/SearchResults";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const {url} = useSelector((state) => state.home);
+  console.log(url);
+
+  useEffect(()=>{
+    apiTesting()
+  },[])
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((res)=>{
+        console.log(res)
+        dispatch(getApiConfiguration(res));
+      })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
