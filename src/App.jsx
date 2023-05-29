b60bb7f52152b1cb0b5ec8757f57eef1e43315cf
Mateo <mateo2823@hotmail.com>
2023-05-29 02:04:37 -0500

    make hero

--- src/App.jsx
+++ src/App.jsx
@@ -23,24 +23,29 @@ function App() {
   },[])
 
   const apiTesting = () => {
-    fetchDataFromApi('/movie/popular')
-      .then((res)=>{
-        console.log(res)
-        dispatch(getApiConfiguration(res));
-      })
+    fetchDataFromApi('/configuration').then((res)=>{
+      console.log(res);
+
+      const url = {
+        backdrop: res.images.secure_base_url + 'original',
+        poster: res.images.secure_base_url + 'original',
+        profile: res.images.secure_base_url + 'original'
+      }
+      dispatch(getApiConfiguration(url));
+    })
   }
 
   return (
     <BrowserRouter>
-      <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:mediaType/:id" element={<Details />} />
-          <Route path="/search/:query" element={<SearchResult />} />
-          <Route path="/explore/:mediaType" element={<Explore />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      <Footer />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+
     </BrowserRouter>
   )
 }
