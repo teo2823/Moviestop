08f7b97bdf3954c9111dda2a500ff08cd58dacb8
Mateo <mateo2823@hotmail.com>
2023-05-31 21:44:40 -0500

    fiish sections movies

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -10,8 +10,9 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../animationLoadImage/img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
-import Genres from "../genres/Genres";
 import "./style.scss";
+import Genres from "../genres/Genres";
+import TopRated from "../../pages/home/topRated/TopRated";
 
 
 
@@ -19,7 +20,17 @@ const Carousel = ({ data, loading }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
-  const navigation = (dir) => {};
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+    
+
+    const scrollAmount = dir === "left" ? container.scrollLeft - (container.offsetWidth + 20) : container.scrollLeft + (container.offsetWidth + 20);
+    container.scrollTo({
+        left: scrollAmount,
+        behavior: 'smooth'
+    });
+
+  };
 
   const skItem = () => {
     return (
